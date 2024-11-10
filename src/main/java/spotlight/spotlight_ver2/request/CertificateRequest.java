00c51e686d27cb793cba788f0826e14c781d86e7
HJ <gmlwnchlrh@gmail.com>
2024-11-10 23:30:15 +0900

    feat: CertificateRequest에 username 필드 추가

--- src/main/java/spotlight/spotlight_ver2/request/CertificateRequest.java
+++ src/main/java/spotlight/spotlight_ver2/request/CertificateRequest.java
@@ -2,10 +2,13 @@ package spotlight.spotlight_ver2.request;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @Setter
+@Component
 public class CertificateRequest {
     private MultipartFile certificate;
+    private String username;
 }
