aa2ff0a646e176609da2d598a4f0edab1e7dff42
jueunk617 <juuuune62@naver.com>
2024-11-23 00:35:23 +0900

    refactor: ExhibitionMapper 수정

--- src/main/java/spotlight/spotlight_ver2/mapper/ExhibitionMapper.java
+++ src/main/java/spotlight/spotlight_ver2/mapper/ExhibitionMapper.java
@@ -15,24 +15,37 @@ public interface ExhibitionMapper {
     ExhibitionMapper INSTANCE = Mappers.getMapper(ExhibitionMapper.class);
 
     // Exhibition -> ExhibitionDTO
-    @Mapping(target = "user.id", source = "user.id")
-    @Mapping(target = "feed", source = "feed", qualifiedByName = "mapFeedToExhibitionFeedDTO")
+    @Mapping(source = "exhibitionId", target = "exhibitionId")
+    @Mapping(source = "location", target = "location")
+    @Mapping(source = "schedule", target = "schedule")
+    @Mapping(source = "time", target = "time")
+    @Mapping(source = "user", target = "user", qualifiedByName = "mapUserIdToExhibitionUserDTO")
+    @Mapping(source = "feed", target = "feed", qualifiedByName = "mapFeedToExhibitionFeedDTO")
     ExhibitionDTO toDTO(Exhibition exhibition);
 
     // ExhibitionDTO -> Exhibition
-    @Mapping(target = "user", source = "user", qualifiedByName = "mapUserIdToUser")
+    @Mapping(source = "user", target = "user", qualifiedByName = "mapUserIdToUser")
     @Mapping(target = "feed", ignore = true)
     Exhibition toEntity(ExhibitionDTO exhibitionDTO);
 
     // Exhibition -> ExhibitionResponse
-    @Mapping(source = "user.id", target = "userId")
-    ExhibitionResponse toResponse(Exhibition exhibition);
+    default ExhibitionResponse toResponse(Exhibition exhibition) {
+        ExhibitionDTO exhibitionDTO = toDTO(exhibition);
+        return new ExhibitionResponse(true, exhibitionDTO); // success를 기본 값으로 설정
+    }
 
     // Feed -> ExhibitionFeedDTO
     @Named("mapFeedToExhibitionFeedDTO")
     default ExhibitionDTO.ExhibitionFeedDTO mapFeedToExhibitionFeedDTO(Feed feed) {
         if (feed == null) return null;
         return new ExhibitionDTO.ExhibitionFeedDTO(feed.getFeedId(), feed.getTitle());
+    }
+
+    // User -> ExhibitionUserDTO
+    @Named("mapUserIdToExhibitionUserDTO")
+    default ExhibitionDTO.ExhibitionUserDTO mapUserIdToExhibitionUserDTO(User user) {
+        if (user == null) return null;
+        return new ExhibitionDTO.ExhibitionUserDTO(user.getId());
     }
 
     // User의 ID를 통해 User 엔티티 생성
